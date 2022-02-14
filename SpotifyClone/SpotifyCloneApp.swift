ae94d2c4ad54410c45ebc8f1630f23b4460ba916
Mickaël MAS <appiwedia@gmail.com>
2022-02-14 14:18:36 +0100

    Initial Commit

---
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by Mickael Mas on 14/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
